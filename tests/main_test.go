6ca64ae1ddb352a3fe1e6ffc7320390dfabb1627
Demetrius <25155822+Laelapa@users.noreply.github.com>
2025-12-08 00:33:17 +0200

    Develop TestMain function to init postgres testcontainer, run migrations on it, and create connection pool for use by the test suite.

---
+++ tests/main_test.go
@@ -0,0 +1,67 @@
+package tests
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pressly/goose/v3"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+var testDBPool *pgxpool.Pool
+
+//nolint:forbidigo // Allow fmt in test main
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	// Init postgres testcontainer
+	pgTC, err := postgres.Run(
+		ctx,
+		"postgres:18-alpine",
+		postgres.WithDatabase("testdb"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("password"),
+		postgres.BasicWaitStrategies(),
+	)
+	if err != nil {
+		fmt.Printf("failed to start container: %s\n", err)
+		os.Exit(1)
+	}
+
+	conStr, _ := pgTC.ConnectionString(ctx, "sslmode=disable")
+
+	// Open connection to run migrations
+	gooseCon, err := sql.Open("pgx", conStr)
+	if err != nil {
+		fmt.Printf("failed to connect to database: %s\n", err)
+		os.Exit(1)
+	}
+
+	if gErr := goose.Up(gooseCon, "../internal/migrations"); gErr != nil {
+		fmt.Printf("failed to run migrations: %s\n", gErr)
+		os.Exit(1)
+	}
+	gooseCon.Close()
+
+	// Open pgxpool for tests
+	testDBPool, err = pgxpool.New(ctx, conStr)
+	if err != nil {
+		fmt.Printf("failed to create pgxpool: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Run tests
+	code := m.Run()
+
+	// Teardown
+	testDBPool.Close()
+	if err := pgTC.Terminate(ctx); err != nil {
+		fmt.Printf("failed to terminate container: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(code)
+}
